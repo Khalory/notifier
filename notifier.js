c8e285a3d4cbee61a02f1fb9378c6c3c70e81c77
Khalory <tk100794@gmail.com>
2016-03-02 00:45:15 +0000

    Added email support through gmail API

--- notifier.js
+++
@@ -1,7 +0,0 @@
-var aggregator = require('./aggregator.js')
-var db = require('./database.js')
-
-aggregator.aggregate((type, name, newData, mes)  => {
-	console.log('NOTIFY CALLBACK ' + mes)
-	db.updateCache(type, name, newData, mes)
-})
